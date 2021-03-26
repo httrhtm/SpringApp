d7d4f1a4e9c6aba02e933d660cb3ccbd991ad2d6
httrhtm <h_hattori@emyus.jp>
2021-03-26 11:19:59 +0900

    一旦コミット

--- src/main/java/com/example/campus/controller/RegisterController.java
+++ src/main/java/com/example/campus/controller/RegisterController.java
@@ -3,18 +3,15 @@ package com.example.campus.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.SessionAttributes;
 
 import com.example.campus.entity.Questions;
 import com.example.campus.service.QuestionService;
 
 //フロントエンドとサーバーサイドの入出力の管理を行う
 @Controller
-@SessionAttributes(types = Questions.class)
 public class RegisterController {
 
 	/**
@@ -24,20 +21,13 @@ public class RegisterController {
 	private QuestionService service;
 
 	/**
-	 * 入力値を取得してconfirmに返す
-	 * @return 入力値
-	 */
-	@ModelAttribute("questionForm")
-	public Questions getQuestion() {
-		return new Questions();
-	}
-
-	/**
 	 * 登録画面に遷移する
 	 * @return 登録画面へのパス
 	 */
+	//question
 	@RequestMapping("/register")
-	public String register(@ModelAttribute("questionForm") Questions questions) {
+	public String register(Model model) {
+		model.addAttribute("questionForm", new Questions());
 		return "register";
 	}
 
@@ -45,13 +35,10 @@ public class RegisterController {
 	 * 確認画面に遷移する
 	 * @return 確認画面へのパス
 	 */
+	//question
 	@PostMapping("/confirm")
-	public String confirm(@ModelAttribute("questionForm") Questions questions) {
-		return "confirm";
-	}
-
-	@GetMapping("/confirm")
-	public String getData(@ModelAttribute("questionForm") Questions questions) {
+	public String confirm(@ModelAttribute Questions questions, Model model) {
+		model.addAttribute("questionForm", questions);
 		return "confirm";
 	}
 
@@ -59,17 +46,14 @@ public class RegisterController {
 	 * 一覧画面に遷移する
 	 * @return 一覧画面へのパス
 	 */
+	//question
 	@PostMapping("/insert")
 	public String insertQuestin(@ModelAttribute Questions questions, Model model) {
-		service.insertQuestion(questions); //DBにinsert
+		//insert
+		service.insertQuestion(questions);
 		return "redirect:/list";
 	}
 
-	@GetMapping("/insert")
-	public String insert(@ModelAttribute Questions questions, Model model) {
-		service.insertQuestion(questions); //DBにinsert
-		return "redirect:/list";
-	}
 
 
 }
--- src/main/java/com/example/campus/repository/QuestionRepository.java
+++ src/main/java/com/example/campus/repository/QuestionRepository.java
@@ -8,7 +8,7 @@ import org.springframework.stereotype.Repository;
 import com.example.campus.entity.Questions;
 
 
-//データベースへアクセスするためのクラス
+//データベースへアクセスするためのインターフェース
 @Repository // springのDIの対象となる
 @Mapper // MybatisでxmlのSQLが対応づけられる
 public interface QuestionRepository {
